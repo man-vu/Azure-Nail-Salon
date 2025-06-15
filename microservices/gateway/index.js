44be4f42f7d46611835478b16e17bb64b68774ce
Man Vu <33613479+man-vu@users.noreply.github.com>
2025-06-15 01:06:43 -0400

    feat: add env config for production

--- microservices/gateway/index.js
+++ microservices/gateway/index.js
@@ -8,14 +8,45 @@ dotenv.config();
 const app = express();
 app.use(cors({ origin: '*', credentials: true }));
 
+const location = process.env.AZURE_LOCATION || 'westus2';
+const prod = process.env.NODE_ENV === 'production';
+
 const services = {
-  auth: process.env.AUTH_URL || 'http://localhost:3002',
-  services: process.env.SERVICE_URL || 'http://localhost:3003',
-  bookings: process.env.BOOKING_URL || 'http://localhost:3004',
-  designers: process.env.DESIGNER_URL || 'http://localhost:3005',
-  transactions: process.env.TRANSACTION_URL || 'http://localhost:3006',
-  reviews: process.env.REVIEW_URL || 'http://localhost:3007',
-  gallery: process.env.GALLERY_URL || 'http://localhost:3008'
+  auth:
+    process.env.AUTH_URL ||
+    (prod
+      ? `http://auth-cg.${location}.azurecontainer.io:3002`
+      : 'http://localhost:3002'),
+  services:
+    process.env.SERVICE_URL ||
+    (prod
+      ? `http://service-cg.${location}.azurecontainer.io:3003`
+      : 'http://localhost:3003'),
+  bookings:
+    process.env.BOOKING_URL ||
+    (prod
+      ? `http://booking-cg.${location}.azurecontainer.io:3004`
+      : 'http://localhost:3004'),
+  designers:
+    process.env.DESIGNER_URL ||
+    (prod
+      ? `http://designer-cg.${location}.azurecontainer.io:3005`
+      : 'http://localhost:3005'),
+  transactions:
+    process.env.TRANSACTION_URL ||
+    (prod
+      ? `http://transaction-cg.${location}.azurecontainer.io:3006`
+      : 'http://localhost:3006'),
+  reviews:
+    process.env.REVIEW_URL ||
+    (prod
+      ? `http://review-cg.${location}.azurecontainer.io:3007`
+      : 'http://localhost:3007'),
+  gallery:
+    process.env.GALLERY_URL ||
+    (prod
+      ? `http://gallery-cg.${location}.azurecontainer.io:3008`
+      : 'http://localhost:3008'),
 };
 
 app.use('/auth', createProxyMiddleware({ target: services.auth, changeOrigin: true }));
